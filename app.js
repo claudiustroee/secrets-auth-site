0f2997f91c48d48943c20e8800342238d5864273
Claudiu-Mihail Stroe <claudiu.stroee@gmail.com>
2022-05-09 17:39:56 +0300

    add bcrypt to password

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const app = express();
 
 app.use(express.static("public"));
@@ -20,8 +20,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -38,18 +36,22 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-   const newUser = new User({
-       email: req.body.username,
-       password: req.body.password
-   }) 
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        }) 
+     
+        newUser.save(function(err){
+             if (err) {
+                 console.log(err);
+             } else {
+                 res.render("secrets");
+             }
+        });
+    });
 
-   newUser.save(function(err){
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-   });
+   
 });
 
 app.post("/login", function(req, res){
@@ -61,9 +63,11 @@ app.post("/login", function(req, res){
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result === true){
+                        res.render("secrets");
+                    }
+                });
             }
         }
     });
